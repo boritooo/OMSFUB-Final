34fbe715311ddc0c87e5b908c896fb76b7a5fc9c
unknown <januarthurpaler27@gmail.com>
2025-04-30 19:20:27 +0800

    sql

--- AMSUFB/settings.py
+++ AMSUFB/settings.py
@@ -98,7 +98,14 @@ DATABASES = {
     }
 }
 database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 
 
 # Password validation
