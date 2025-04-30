1717343e417146b7f0fa0180935a55d32463c59e
unknown <januarthurpaler27@gmail.com>
2025-04-30 18:54:58 +0800

    commit sql

--- AMSUFB/settings.py
+++ AMSUFB/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,7 +97,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+DATABASES = {
+    'default': dj_database_url.config(default='postgresql://amsfub_master_user:n3GRm4av1oiazU0uFfS31jRn022jbvh6@dpg-d08rfu2dbo4c73e7rhtg-a.singapore-postgres.render.com/amsfub_master')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
